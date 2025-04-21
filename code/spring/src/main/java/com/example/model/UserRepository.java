a8870386aea8a1e340e95e4f0550cac0783ee525
yakovliam <jacob@yakovliam.com>
2025-04-21 15:14:23 -0400

    feat: update

--- code/spring/src/main/java/com/example/model/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.model;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
-}
