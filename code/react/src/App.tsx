56561d6425b25ec89c3ef6255179c5f33f2b7df5
yakovliam <jacob@yakovliam.com>
2025-04-21 15:41:27 -0400

    feat: update

--- code/react/src/App.tsx
+++ code/react/src/App.tsx
@@ -1,7 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router";
 import AuthenticationContextWrapper from "@/components/auth/AuthenticationContextWrapper";
 import HomePage from "@/pages/home/HomePage";
-import Layout from "./components/auth/layout/Layout";
+import Layout from "@/components/auth/layout/Layout";
+import useEasyAuth from "@/hooks/use-easy-auth";
+import Loading from "./components/loading/Loading";
 
 function App() {
   return (
@@ -10,10 +12,54 @@ function App() {
         <Route element={<AuthenticationContextWrapper />}>
           <Route element={<Layout />}>
             <Route path="/" element={<HomePage />} />
-          </Route></Route>
+
+            <Route element={<ReverseProtectedRoute />}>
+              <Route path="/login" element={<h1>Login Page</h1>} />
+            </Route>
+
+            <Route element={<ProtectedRoute />}>
+              <Route path="/protected" element={<h1>Protected Page</h1>} />
+            </Route>
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
+
+/**
+ * ProtectedRoute is a route that is only accessible to authenticated users.
+ */
+const ProtectedRoute = () => {
+  const { isAuthenticated, isLoading } = useEasyAuth();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
+
+/**
+ * ReverseProtectedRoute is a route that is only accessible to unauthenticated users.
+ */
+const ReverseProtectedRoute = () => {
+  const { isAuthenticated, isLoading } = useEasyAuth();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
+};
+
 
 export default App;
