119a75ce0eacc96f8e2840eb37940ac7cb0826e8
yakovliam <jacob@yakovliam.com>
2025-04-21 15:27:42 -0400

    feat: update

--- code/react/src/main.tsx
+++ code/react/src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { AuthProvider } from "react-oidc-context";
+
+const oidcConfig = {
+  authority: import.meta.env.VITE_AUTHORITY,
+  client_id: import.meta.env.VITE_CLIENT_ID,
+  redirect_uri: `${window.location.origin}${window.location.pathname}`,
+  post_logout_redirect_uri: window.location.origin,
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...oidcConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>
 );
