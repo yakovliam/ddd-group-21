85b4dc2bf007ff3309260e593b7caa4e0f485ce1
yakovliam <jacob@yakovliam.com>
2025-04-21 16:17:43 -0400

    feat: update loginpage

--- code/react/src/pages/login/LoginPage.tsx
+++ code/react/src/pages/login/LoginPage.tsx
@@ -1,18 +1,12 @@
 import Button from "@/components/ui/Button";
-import Input from "@/components/ui/Input";
 import useEasyAuth from "@/hooks/use-easy-auth";
-import { useState } from "react";
 
 const LoginPage = () => {
   const { authContext } = useEasyAuth();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
   const redirectToLogin = () => {
-    authContext.signinResourceOwnerCredentials({
-      username: username,
-      password: password,
+    authContext.signinRedirect({
+      scope: "untrusted-audience",
     });
   };
 
@@ -20,19 +14,6 @@ const LoginPage = () => {
     <div className="flex flex-col items-center gap-4">
       <h1>Login Page</h1>
       <Button onClick={redirectToLogin}>Login</Button>
-
-      <div className="flex flex-col gap-2 w-sm">
-        <Input
-          type="text"
-          placeholder="Username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
     </div>
   );
 };
